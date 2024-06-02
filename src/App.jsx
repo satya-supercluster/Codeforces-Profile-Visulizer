8c30e176276187fa1cce8f7aade04668af69b7a3
satya-supercluster <satya.supercluster@gmail.com>
2024-06-02 14:07:56 +0530

    Project-Setup and Navbar done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <div className="py-5 px-3 sm:px-5 backdrop-blur-sm h-screen">
+        <Navbar />
+        <h1>Helo</h1>
+      </div>
+    </>
+  );
+}
